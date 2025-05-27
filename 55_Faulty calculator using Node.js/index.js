c02159f74dee537bb9a12828a7d23aaf8cef0f7b
roshandev1 <roshanark1010@gmail.com>
2025-05-27 13:09:34 -0700

    Faulty Calculator using JS

---
+++ 55_Faulty calculator using Node.js/index.js
@@ -0,0 +1,46 @@
+/*
+Create a Faulty Calculator:
+
+This Faulty calc does the following :
+1. It takes 2 numbers as input.
+2. It performs wrong operation as follows;
+    
+   + as -
+   * as +
+   - as /
+   / as **
+
+   It performs wrong operation 10% of the times.
+*/
+
+let random = Math.random();
+console.log(random);
+
+let a = prompt("Enter first number : ");
+let b = prompt("Enter Operation to perform : ");
+let c = prompt("Enter second number : ");
+
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**",
+}
+
+if(random>0.1){
+    //perform correct calculation (this is 90% of the times)
+
+    console.log(`The result is ${eval(`${a} ${b} ${c}`)}`);
+    //eval() is not recommeded to use more in products.
+    //This is a easier method to solve problems.
+
+}
+
+else
+    //Perform incorrect calculation (this is 10% of the times) 
+    
+   c = obj(c);
+
+   console.log(`The result is ${eval(`${a} ${b} ${c}`)}`);
+
+}
